258a3073e73373e537947ace4ab6f73a3d60654f
ria0864 <dbwls08640@gmail.com>
2019-05-19 22:26:53 +0900

    feat(test): travisCI test
    reference 디렉터리 하위에 travisCI test import

---
+++ reference/travisci-test/src/travisCI/main.java
@@ -0,0 +1,8 @@
+package travisCI;
+import common.Member;
+public class main {
+    public static void main(String[] args) {
+        Member mem = new Member("yujin","email@gmail.com");
+        System.out.println(mem.getOut());
+    }
+}
