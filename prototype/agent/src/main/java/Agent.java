a6297d99c239dcd523fb6b4685fc8985de33f827
taehee_jung <taehee_jung@tmax.co.kr>
2019-05-27 20:25:43 +0900

    feat(agent): JavaAgent Class Rename

--- prototype/agent/src/main/java/Agent.java
+++ prototype/agent/src/main/java/Agent.java
@@ -2,7 +2,7 @@ package main.java;
 
 import java.lang.instrument.Instrumentation;
 
-public class JavaAgent {
+public class Agent {
 	public void premain(String args, Instrumentation inst) {
 
 		inst.addTransformer(new Transformer());
